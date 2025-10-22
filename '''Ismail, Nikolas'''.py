6f4d65db56beee2aefad33a187339cce39a96d06
nikphobia <6300062@edu.vaniercollege.qc.ca>
2025-10-22 12:16:10 -0400

    part 3 answers

--- '''Ismail, Nikolas'''.py
+++ '''Ismail, Nikolas'''.py
@@ -3,5 +3,30 @@
 import pandas as pd
 import seaborn as sns
 
+# (3)
 data=pd.read_csv("wdi_wide.csv")
-b=pd.info(data["Physicians","Population"])
+b= data.info(
+                     
+# (4)
+c= data.nunique()
+print(c)
+
+# (5)
+d= data.describe()
+print(d)
+
+# this function gives different parameters such as count, mean, standard deviation etc of all the different columns in the dataset
+
+# (6)
+
+
+data["GNI per Capita"] = pd.DataFrame.round(data["GNI"] / data ["Population"])
+f= data["GNI per Capita"]
+print(f)
+#e= pd.DataFrame.round(data["GNI per Capita"])                     
+
+# (7) 
+# a)
+
+h= data["Region"]
+g= data.value_counts() 
---
+++ part 3.py
@@ -0,0 +1,60 @@
+#part 3 (1)
+#
+import pandas as pd
+import seaborn as sns
+
+# (2)
+data=pd.read_csv("wdi_wide.csv")
+
+print ("3.___________________________________________________")
+# (3)
+info= data.info()
+print ("4.___________________________________________________")
+# (4)
+unique= data.nunique()
+print(unique)
+
+print ("5.___________________________________________________")
+
+# (5)
+describe = data.describe()
+print(describe)
+
+print ("6.___________________________________________________")
+
+# (6)
+data["GNI per Capita"] = pd.DataFrame.round(data["GNI"] / data ["Population"])
+GNI_Cap= data["GNI per Capita"]
+print(GNI_Cap)
+
+print ("7.___________________________________________________")
+
+# (7)
+#a
+
+value_region = pd.DataFrame(data["Region"].value_counts())
+
+print(value_region)
+#b
+
+value_eco = pd.DataFrame(data["High Income Economy"].value_counts())
+print(value_eco)
+
+print ("8.___________________________________________________")
+
+# (8)
+df = pd.DataFrame(data)
+cross= pd.crosstab(df['High Income Economy'], df['Region'])
+print(cross)
+
+print ("9.___________________________________________________")
+
+#(9)
+number=0
+n=1
+for i in df["Life expectancy, female"]:
+    if i > 80:
+        print(df["Country Name"][n],"life expectancy:", i)
+        n += 1
+        number +=1
+print ("number of countries: ", number)
